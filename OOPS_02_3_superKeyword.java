52cc8f9d56e935813be1c64a53e5e4e511accbd8
Sourav163 <souravroutray2016@gmail.com>
2024-12-19 22:46:33 +0530

    initial commit

---
+++ OOPS_02_3_superKeyword.java
@@ -0,0 +1,26 @@
+package Java_with_Anuj_Bhaiya;
+
+public class OOPS_02_3_superKeyword {
+    public static void main(String[] args) {
+        Auto a = new Auto();
+        System.out.println(a.wheels);
+        a.wheels = 4;
+        System.out.println(a.wheels);
+    }
+}
+
+class Auto extends Vehicle {
+    public Auto() {
+//         super(); --> By Default
+        // super - Accesses the immediate parent class's properties/behaviors.
+        super(3); // Overrides the default super()
+        System.out.println("Auto Created.");
+        super.start(); // Accesses the immediate parent class's ("Vehicle") start()
+        start();
+        System.out.println("Auto Wheels = " + super.wheels); // Same as "wheels"
+    }
+
+    public void start() {
+        System.out.println("Auto is starting...");
+    }
+}
