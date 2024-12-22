8809f230cb788a2d8614a3007e690d8455edd5fc
Sourav163 <souravroutray2016@gmail.com>
2024-12-22 22:57:53 +0530

    initial commit

---
+++ OOPS_03_1_Package.java
@@ -0,0 +1,6 @@
+// To define the folder as a package, use the package keyword like following
+// It means that the current java file is in the package/folder
+package Java_with_Anuj_Bhaiya;
+
+public class OOPS_03_1_Package {
+}
