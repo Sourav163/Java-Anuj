6734c0d71a8f57e2f03404b9ad9b9ec8888a3327
Sourav163 <souravroutray2016@gmail.com>
2024-12-23 23:13:34 +0530

    added package importing

--- OOPS_03_1_Package.java
+++ OOPS_03_1_Package.java
@@ -1,6 +1,19 @@
-// To define the folder as a package, use the package keyword like following
+// To define the folder as a package, use the "package" keyword like following
 // It means that the current java file is in the package/folder
 package Java_with_Anuj_Bhaiya;
 
+// To import other package(s), use the "import" keyword like following
+// It must be declared in between "package" and "class"
+//import java.util.Scanner;
+//import java.util.List;
+//import java.util.ArrayList; --> can be imported in multiple lines
+// But, for better optimization it can be written in a single line using "*"
+
+import java.util.*; // It imports all the classes available in the package "java.util"
+
 public class OOPS_03_1_Package {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> li = new ArrayList<>();
+    }
 }
