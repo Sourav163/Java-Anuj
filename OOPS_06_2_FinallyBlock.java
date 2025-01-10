da9bee38e62d1e69caecbf390c5e3c32aaac1e00
Sourav Routray <souravroutray2016@gmail.com>
2025-01-11 00:06:48 +0530

    initial commit

---
+++ OOPS_06_2_FinallyBlock.java
@@ -0,0 +1,24 @@
+package Java_with_Anuj_Bhaiya;
+
+public class OOPS_06_2_FinallyBlock {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+
+        System.out.println("Hii");
+
+        // try block can be one.
+        try {
+            System.out.println(a[8]);
+        }
+        // catch blocks can be multiple.
+//        catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+        // finally block can be one.
+        finally {
+            System.out.println("Finally: I'll run always.");
+        }
+
+        System.out.println("Bye");
+    }
+}
