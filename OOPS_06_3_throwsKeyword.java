3adbc7da1e17f1dd031002f30d8f0a6b487882ff
Sourav Routray <souravroutray2016@gmail.com>
2025-01-12 00:37:31 +0530

    initial commit

---
+++ OOPS_06_3_throwsKeyword.java
@@ -0,0 +1,18 @@
+package Java_with_Anuj_Bhaiya;
+
+public class OOPS_06_3_throwsKeyword {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int a = 8;
+        try {
+            System.out.println(getNumberFromArray(arr, a));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+
+    // throws - informs that the method might give an exception that is mentioned.
+    static int getNumberFromArray(int[] arr, int a) throws ArrayIndexOutOfBoundsException {
+        return arr[8];
+    }
+}
