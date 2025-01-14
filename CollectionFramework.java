5a633a286012bf0e9fc608ee893e7e7df8ec2a72
Sourav Routray <souravroutray2016@gmail.com>
2025-01-14 23:57:22 +0530

    initial commit

---
+++ CollectionFramework.java
@@ -0,0 +1,11 @@
+package Java_with_Anuj_Bhaiya;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CollectionFramework {
+    public static void main(String[] args) {
+        // creating a list object with arraylist type
+        List<Integer> list = new ArrayList<>();
+    }
+}
